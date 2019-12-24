1d67ff7580a6070c4c9cdc472c847c6848fea9fc
Sam Bonnekamp <samuel@bonnekamp.net>
2019-12-24 14:33:59 +0800

    moved scanfuck() to header file

--- bot.c
+++ bot.c
@@ -7,7 +7,7 @@
 #include <errno.h>
 #include <pthread.h>
 #include "userinput.h" //username and password
-
+#include "scanfuck.h"
 
 struct sockaddr_in twitchaddr;
 int twitchsock;
@@ -59,28 +59,6 @@ char* analyseInput(char* strinput){
   return NULL;
 }
 
-//TODO: move this method to header file
-char* scanfuck(){ //gets char* of 'infinite' length | TODO: make a cap, because you could crash a system with a 9gb string. Could literally use fgets()
-  int len_max = 10;
-  int current_size = len_max;
-
-  char* pStr = malloc(len_max);
-
-  if (pStr != NULL) {
-    int c = EOF;
-    int i = 0;
-    while ((c = fgetc(stdin)) != '\n') {
-      pStr[i++] = (char)c;
-      if (i == current_size){
-	current_size = i + 1; //i++ wouldnt work for some reason
-	pStr = realloc(pStr, current_size);
-      }
-    }
-    pStr[i] = '\0';
-  }
-  return pStr;
-  
-}
 
 
 void readerTHEThread(void* context){
