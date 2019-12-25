0c888eb2dd651a431c48e36ffa35965b323996a0
Sam Bonnekamp <samuel@bonnekamp.net>
2019-12-25 22:25:06 +0800

    issue #20 - moved sendMsg and joinChannel  to header file (`twitch.h`)

--- bot.c
+++ bot.c
@@ -13,23 +13,11 @@ struct sockaddr_in twitchaddr;
 int twitchsock;
 char current[100];
 
+#include "twitch.h"
+
 struct connectionData{
   int sockfd;
 };
-//TODO 20: move sendMsg to headerfile
-void sendMsg(char* payload){
-  if(write(twitchsock, payload, strlen(payload)) == -1){
-	perror("failed to write to socket\n"); 
-      }
-}
-
-//TODO 20: move joinChannels to headerfile
-void joinChannel(char* message){
-  char payload[50];
-  sprintf(payload, "JOIN %s\r\n", message);
-  sendMsg(payload); 
-  sprintf(current, "%s", message);
-}
 
 //I hate this entire function
 char* analyseInput(char* strinput){
