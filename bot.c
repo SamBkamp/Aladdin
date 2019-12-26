9c75ec1acf18fb80978ad568805deb8b0bdfbf37
Sam Bonnekamp <samuel@bonnekamp.net>
2019-12-26 23:30:52 +0800

    moved setup stuff to own method in `twitch.h`

--- bot.c
+++ bot.c
@@ -9,20 +9,10 @@
 #include "userinput.h" //username and password
 #include "scanfuck.h"
 #include <signal.h>
-
-struct sockaddr_in twitchaddr;
-int twitchsock;
-char current[100];
-
 #include "twitch.h"
 
-struct connectionData{
-  int sockfd;
-  pthread_t writerThread;
-  pthread_t readerThread;
-};
-
 struct connectionData *connData;
+
 
 //I hate this entire function
 char* analyseInput(char* strinput){
@@ -106,74 +96,29 @@ void* writerTHEThread(void* context){
 
 
 int main(){
-  //here to joinChannel could be put in its own function in the twitch.h file?
   struct connectionData conData;
-  twitchsock = socket(AF_INET, SOCK_STREAM, 0);
-  if(twitchsock == -1){
-    printf("couldn't connect to socket");
-    exit(0);
-  }
-  //printf("connected to socket\n");
-  struct hostent* host = gethostbyname("irc.chat.twitch.tv");
-  if(host == NULL){
-    printf("error connecting to twitch servers");
-    exit(0);
 
-  }
-  //setup addresses
-  bzero(&twitchaddr, sizeof(twitchaddr));
-  twitchaddr.sin_family = AF_INET;
-  twitchaddr.sin_addr.s_addr = *(long *)host->h_addr_list[0];
-  twitchaddr.sin_port = htons(6667);
-  conData.sockfd = twitchsock;
-   if (connect(twitchsock, (struct sockaddr*)&twitchaddr, sizeof(twitchaddr)) != 0) {
-    printf("-----[failed to connect to server]-----\n");
-    exit(0);
-  }
+  setup();
+  joinChannel("#bkamp_");
   
-  //inital login to twitch irc servers
-   char buff[1000];
-   char payload[100] = "PASS ";
-   //this entire strcat section will be changed when get config files/db implemented
-   //TODO 17: change this to sprintf lmao
-   strcat(payload, password);
-   strcat(payload, "\r\nNICK ");
-   strcat(payload, nick);
-   strcat(payload, "\r\n");
-   
-   if(write(twitchsock, payload, strlen(payload)) == -1){
-     printf("failed to write to socket \n");
-   }
-   
-   
-   read(twitchsock, buff, sizeof(buff));
-   printf("%s", buff);
-
-   joinChannel("#bkamp_");
-   
-   bzero(buff, sizeof(buff));
-   
-   read(twitchsock, buff, sizeof(buff));
-   printf("%s", buff);
   
-
-   pthread_t writerThread;
-   pthread_t readerThread;
-   
-
-   pthread_create(&writerThread, NULL, writerTHEThread, (void *) &conData);
-   pthread_create(&readerThread, NULL, readerTHEThread, (void *) &conData);
-
-   conData.writerThread = writerThread;
-   conData.readerThread = readerThread;
-
-   connData = &conData;
-   
-   pthread_join(writerThread, NULL);
-   sleep(1);
-   pthread_join(readerThread, NULL);
-
-   
+  pthread_t writerThread;
+  pthread_t readerThread;
+  
+  
+  pthread_create(&writerThread, NULL, writerTHEThread, (void *) &conData);
+  pthread_create(&readerThread, NULL, readerTHEThread, (void *) &conData);
+  
+  conData.writerThread = writerThread;
+  conData.readerThread = readerThread;
+  
+  connData = &conData;
+  
+  pthread_join(writerThread, NULL);
+  sleep(1);
+  pthread_join(readerThread, NULL);
+  
+  
   return 0;
 }
 
