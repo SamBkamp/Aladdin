f8e6dbbf016de15e8c13b1d6cbc282600dfe39c3
Sam Bonnekamp <samuel@bonnekamp.net>
2020-01-01 22:14:52 +0800

    issue #38 - user data is parsed from userinfo.txt

---
+++ parseInfo.h
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define inputfile "userinfo.txt"
+
+
+char password[50];
+char nick[50];
+
+int parseInfo(){
+  
+  FILE* fp = fopen(inputfile, "r");
+
+  if (fp == NULL){
+    printf("couldn't open userinfo.txt");
+    return -1;
+  }
+
+  char buffer[2048];
+  while(fgets(buffer, 2048, fp)!= NULL){ //goes over file line by line
+    sscanf(buffer, "%[^\n]", buffer);
+    char buff[10];
+    for(int i = 0; i < strlen(buffer) && buffer[i] != ':'; i++){ //parses character at ':'
+      buff[i] = buffer[i];
+      buff[i+1] = 0;
+    }
+    if(strcmp(buff, "nick")!=0 && strcmp(buff, "pass")!=0){ //checks to make sure prefix is correct
+      printf("incorrectly formatted userinfo.txt\n%s\n", buff);
+      return -1;
+      }
+    
+    if(strcmp(buff, "nick")==0){ //assigns correct value to password || nick
+      strtok(buffer, ":");
+      strcpy(nick, strtok(NULL, ":"));
+    }else{
+      strtok(buffer, ":");
+      strcpy(password, strtok(NULL, ":"));
+    }
+  }
+  printf("nick is: %s\npass is: %s\n", nick, password);
+  return 1;
+}
+
