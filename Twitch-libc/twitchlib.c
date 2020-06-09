ac5ed533067aa9e1a8995c5210a8008de4971da5
Sam Bonnekamp <sam@bonnekamp.net>
2020-06-09 14:01:31 +0800

    updated twitch-libc to version 0.3.1

--- Twitch-libc/twitchlib.c
+++ Twitch-libc/twitchlib.c
@@ -10,7 +10,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
    |                                   | 
    |  @author: sam@bonnekamp.net       |
    |                                   |
-   |  @version: 0.2.1                  |
+   |  @version: 0.3.1                  |
    |                                   |  
    +-----------------------------------+
 */
